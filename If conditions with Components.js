553fe4e0a79122ea1e49fce057bf37d821b5cdb0
Can Taner TARHAN <cantanert@gmail.com>
2018-09-28 16:51:48 +0300

    Create If conditions with Components.js

---
+++ If conditions with Components.js
@@ -0,0 +1,29 @@
+
+const fiftyFifty = Math.random() < 0.5;
+class TonightsPlan extends React.Component{
+    render(){
+        if(fiftyFifty){
+   			    return ( 
+            <h1>Tonight I'm going out WOOO</h1> 
+            ); 
+        }else{
+    		    return ( 
+            <h1>Tonight I'm going to bed WOOO</h1> 
+            );
+        }	
+    }
+};
+ReactDOM.render(<TonightsPlan/>,document.getElementById('app'));
+
+// FUNCTION PASSING IN COMPONENT RENDER OPERATION
+
+class Button extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  render() {
+    return <button onClick = {this.scream} >AAAAAH!</button>;
+  }
+}
+ReactDOM.render(<Button/>,document.getElementById('app'));
