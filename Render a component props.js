39fb6f1474a05fd8a8c082236830164e9259b107
Can Taner TARHAN <cantanert@gmail.com>
2018-09-28 16:58:48 +0300

    Create Render a component props.js

---
+++ Render a component props.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting firstName='Alexei' />, 
+  document.getElementById('app')
+);
+
+
